75869ccb80706e527552ab7124e1a219a2e0b8e2
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-06-03 20:15:38 +0200

    Update Countries.test.js

--- src/tests/Countries.test.js
+++ src/tests/Countries.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import Countries from '../components/pages/countries.js';
+import Countries from '../components/pages/countries';
 import store from '../redux/store'; // Replace with your actual Redux store import
 
 test('renders Country component correctly', () => {
